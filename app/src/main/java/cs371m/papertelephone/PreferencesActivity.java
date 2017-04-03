a059b4b66d95cddef7196117f8352bdc6c2d684a
David Shi <dshi@utexas.edu>
2017-04-02 22:43:19 -0500

    roundsSummary listener and updater

--- app/src/main/java/cs371m/papertelephone/PreferencesActivity.java
+++ app/src/main/java/cs371m/papertelephone/PreferencesActivity.java
@@ -2,6 +2,7 @@ package cs371m.papertelephone;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 
@@ -18,16 +19,46 @@ public class PreferencesActivity extends AppCompatActivity {
     }
 
     public static class SettingsFragment extends PreferenceFragment {
+        static SharedPreferences sharedPrefs;
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+            sharedPrefs = getActivity().getSharedPreferences("MainActivity", MODE_PRIVATE);
 
             // set up shared preferences file
             getPreferenceManager().setSharedPreferencesName("MainActivity");
-            SharedPreferences sharedPrefs =
-                    getActivity().getSharedPreferences("MainActivity", MODE_PRIVATE);
             // Load the preferences from an XML resource
             addPreferencesFromResource(R.xml.preferences);
+
+            // handle rounds summary and listener
+            roundsListener();
+            updateRoundsSummary();
+        }
+
+        private void roundsListener() {
+            final Preference roundsPref = findPreference("rounds");
+            roundsPref.setOnPreferenceChangeListener(
+                    new Preference.OnPreferenceChangeListener() {
+
+                        @Override
+                        public boolean onPreferenceChange(Preference preference, Object newValue) {
+                            String roundsSummary = "Number of Rounds: " + newValue;
+                            roundsPref.setSummary(roundsSummary);
+
+                            SharedPreferences.Editor editor = sharedPrefs.edit();
+                            editor.putString("rounds", newValue.toString());
+                            editor.apply();
+                            return true;
+                        }
+                    }
+            );
+        }
+
+        private void updateRoundsSummary() {
+            String roundsSummary = "Number of Rounds: " + sharedPrefs.getString("rounds", "3");
+            Preference roundsPref = findPreference("rounds");
+            roundsPref.setSummary(roundsSummary);
         }
     }
 
